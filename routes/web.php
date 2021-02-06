615765b5741c2514bd36a372014ebc0ddf45f0e8
Karla jaime <you@example.com>
2021-02-05 21:06:19 -0700

    Actividad

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\InicioController;
 use App\Http\Controllers\NoticiaController;
-
+use App\Http\Controller\Admin\NoticiaController as AdminNoticiaController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -20,3 +20,16 @@ Route::get('/contacto', [InicioController::class, "contacto"]);
 
 Route::get('/noticias',[NoticiaController::class, "lista"])  -> name("noticias");
 Route::get('/noticias/{id}',[NoticiaController::class, "detalles"]) ->name("noticias.detalles");
+
+/*
+Recuersos
+Crear - create
+Almacenar -store
+Listar - index
+Mostrar detalles - show
+Editar -edit
+actualziar - update
+Elimitar -destroy
+*/
+
+Route::get("/admin/noticias", [AdminNoticiaController::class, "index"])->name("admin.noticias.index");
